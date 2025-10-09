198924b53adfce00f6747fef9ee4d14fcba38234
Operanki223 <fgo239@outlook.jp>
2025-10-10 03:03:37 +0900

    スキルツリーの修正

--- Assets/Scripts/SkillTree/SkillTreeGenerater/SkillTreeManager.cs
+++ Assets/Scripts/SkillTree/SkillTreeGenerater/SkillTreeManager.cs
@@ -111,10 +111,10 @@ public class SkillTreeManager : MonoBehaviour
     }
 
     /// <summary>
-    /// スキル・ステータスの説明をテキストに代入
+    /// スキル・ステータスの説明をテキストに代入(id,説明を表示するかしないか)
     /// </summary>
     /// <param name="id"></param>
-    public void UpdateSkillInfoText(int id, bool canLearned)
+    public void UpdateSkillInfoText(int id, bool canLearned, string newInfo = null)
     {
         string text = "データがありません";
         if (canLearned)
@@ -128,15 +128,16 @@ public class SkillTreeManager : MonoBehaviour
             {
                 if (n.GetId().Equals(id))
                 {
-                    text = n.GetExplain();
+                    text = "スキル:" + n.GetName() + newInfo + "\n" + n.GetExplain() + "\n必要SP:" + n.GetSp() + " 必要MP:" + n.GetMp();
                 }
             }
+
 
             foreach (var n in dataSetting.nodeStatusData)
             {
                 if (n.getId().Equals(id))
                 {
-                    text = n.getExplain();
+                    text = n.getExplain() + newInfo + "\n必要SP:" + n.GetSp();
                 }
             }
         }
@@ -173,6 +174,7 @@ public class SkillTreeManager : MonoBehaviour
     /// <returns></returns>
     public bool CanLearnSkill(int cost, int id)
     {
+        //Debug.Log($"{skillPoint},{cost}");
         if (id == 0) return true;
         if (skillPoint < cost)
         {
@@ -272,10 +274,9 @@ public class SkillTreeManager : MonoBehaviour
             }
         }
 
-        ChechActiveBlocks();
-
         skillPoint -= cost;
 
+        ChechActiveBlocks();
         UpdateSkillPointText();
         SkillStatusLoader.instance.SaveSkillData();// スキルのJSONに保存
         SkillStatusLoader.instance.SaveStatusData();// ステータスのJSONに保存
