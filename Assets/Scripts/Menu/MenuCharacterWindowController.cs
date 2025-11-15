8774d561b83a68d2af97ac04505a15d8734ae9ea
komugi1122 <b1023209@fun.ac.jp>
2025-11-15 18:55:56 +0900

    characterStatusの初期化を適切に変更

--- Assets/Scripts/Menu/MenuCharacterWindowController.cs
+++ Assets/Scripts/Menu/MenuCharacterWindowController.cs
@@ -187,8 +187,6 @@ public class MenuCharacterWindowController : MonoBehaviour, IMenuWindowControlle
         int currentId = CharacterStatusManager.Instance.partyCharacter[_characterIndex];
         var currentCharacterData = CharacterDataManager.Instance.GetCharacterData(currentId);   // キャラクターのデータを持ってくる
         var currentCharacterStatus = CharacterStatusManager.Instance.GetCharacterStatusById(currentId); // キャラクターの現在のステータスを持ってくる
-        var currentParameterTable = CharacterDataManager.Instance.GetParameterTable(currentId); // キャラクターのパラメータテーブルを取得する
-        var currentParameterRecord = currentParameterTable.parameterRecords[currentCharacterStatus.level - 1]; // キャラクターの実際のステータスの値を持ってくる（持ってきたいレベルのステータス＝
 
         _uiController.SetCharacterNameText(currentCharacterData.characterName);
         _uiController.SetLevelValueText(currentCharacterStatus.level);
