881fa0890973768e7f2306d97854ffb07ce6ed13
komugi1122 <b1023209@fun.ac.jp>
2025-12-02 13:03:42 +0900

    スキル/アイテム使用時の画面の挙動を修正

--- Assets/Scripts/Menu/MenuSelectWindowController.cs
+++ Assets/Scripts/Menu/MenuSelectWindowController.cs
@@ -163,6 +163,7 @@ public class MenuSelectWindowController : MonoBehaviour, IMenuWindowController
 
     private void SetPhase2Text()
     {
+        Debug.Log("スキル/アイテム使用フェーズ1");
         string text = string.Empty;
         if (MenuManager.Instance.MenuUsePhase == MenuUsePhase.SkillUse)
         {
@@ -225,6 +226,7 @@ public class MenuSelectWindowController : MonoBehaviour, IMenuWindowController
     // 初期化をここで行う。
     private void InitializeCommand()
     {
+        Debug.Log("スキル/アイテム使用フェーズ０");
         _cursor = 0;
         _selectPhase = 0;
         _uiController.ShowSelectedCursor(_cursor);
