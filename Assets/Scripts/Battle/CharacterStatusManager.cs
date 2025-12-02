dca56331ef7e0ff94335d23b520d0a8126b26324
komugi1122 <b1023209@fun.ac.jp>
2025-12-02 14:26:19 +0900

    スキルツリーでHP/MPアップのパネルを取得し、回復しても、MAXまで回復しない不具合の解消

--- Assets/Scripts/Battle/CharacterStatusManager.cs
+++ Assets/Scripts/Battle/CharacterStatusManager.cs
@@ -168,12 +168,10 @@ public class CharacterStatusManager : DontDestroySingleton<CharacterStatusManage
             Debug.LogWarning($"キャラクターのステータスが見つかりませんでした。 ID : {characterId}");
             return;
         }
-        var parameterTable = CharacterDataManager.Instance.GetParameterTable(characterId);
-        var parameterRecord = parameterTable.parameterRecords.Find(p => p.Level == characterStatus.level);
         characterStatus.currentHp += hpDelta;
-        if (characterStatus.currentHp > parameterRecord.HP)
+        if (characterStatus.currentHp > characterStatus.maxHp)
         {
-            characterStatus.currentHp = parameterRecord.HP;
+            characterStatus.currentHp = characterStatus.maxHp;
         }
         else if (characterStatus.currentHp < 0)
         {
@@ -187,9 +185,9 @@ public class CharacterStatusManager : DontDestroySingleton<CharacterStatusManage
         }
 
         characterStatus.currentMp += mpDelta;
-        if (characterStatus.currentMp > parameterRecord.MP)
+        if (characterStatus.currentMp > characterStatus.maxMp)
         {
-            characterStatus.currentMp = parameterRecord.MP;
+            characterStatus.currentMp = characterStatus.maxMp;
         }
         else if (characterStatus.currentMp < 0)
         {
