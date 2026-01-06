a3528898d60f564b1ac4611eecf7fa2da3ea0a63
komugi1122 <b1023209@fun.ac.jp>
2026-01-06 17:42:39 +0900

    ResourcesフォルダにEngineAssetsフォルダを作ることで、jsonファイルの読み込みに成功するかもしれない

--- Assets/Scripts/Common/SaveUtility.cs
+++ Assets/Scripts/Common/SaveUtility.cs
@@ -13,7 +13,7 @@ public static class SaveUtility
 
         return MessagePackSerializer.Deserialize<T>(msgPackData);
     }
-    
+
     public static void DataToJson<T>(T data, string savePath) where T : class
     {
         //データをJSON形式の文字列にシリアライズし、指定したパスに保存
@@ -29,13 +29,13 @@ public static class SaveUtility
 
         return MessagePackSerializer.Deserialize<T>(msgPackData);
     }
-    
+
     public static void DataToSaveFile<T>(T data, string savePath) where T : class
     {
         byte[] msgPackData = MessagePackSerializer.Serialize(data);
         File.WriteAllBytes(savePath, msgPackData);
     }
-    
+
     public static IFileAssetLoader FileAssetLoaderFactory()
     {
 #if UNITY_WEBGL
